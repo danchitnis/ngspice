445d540e6ef850e717c1f8efdc7b6ec6ebe7c3c3
danchitnis <danchitnis@users.noreply.github.com>
2020-10-22 20:17:14 +0100

    ngspice 33 & emcc 2.0.7

--- emcc/run.sh
+++ emcc/run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo Entryponit script is Running...
 
+NGSPICE_HOME="https://github.com/danchitnis/ngspice-sf-mirror"
+#NGSPICE_HOME="https://git.code.sf.net/p/ngspice/ngspice"
+
+echo -e "Ngsice git repository is $NGSPICE_HOME\n"
+
 cd /opt
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
@@ -13,11 +18,15 @@ echo -e "installing ngspice...\n"
 
 cd /opt
 
-git clone https://git.code.sf.net/p/ngspice/ngspice ngspice-ngspice
+git clone $NGSPICE_HOME ngspice-ngspice
 
 cp ./misc_time.c ./ngspice-ngspice/src/misc
 
 cd ngspice-ngspice
+
+#https://www.cyberciti.biz/faq/how-to-use-sed-to-find-and-replace-text-in-files-in-linux-unix-shell/
+#https://sourceforge.net/p/ngspice/patches/99/
+sed -i 's/-Wno-unused-but-set-variable/-Wno-unused-const-variable/g' ./configure.ac
 
 ./autogen.sh
 mkdir release
@@ -28,6 +37,7 @@ emconfigure ../configure --disable-debug
 wait
 
 emmake make
+#emmake make 2>&1 | tee make.log
 
 wait
 
@@ -36,7 +46,7 @@ wait
 cd src
 mv ngspice ngspice.js
 mkdir -p /mnt/build
-\cp ngspice.js ngspice.wasm /mnt/output
+\cp ngspice.js ngspice.wasm /mnt/build
 
 
 echo -e "\n"
