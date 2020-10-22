530752e77819c00dc510f05c37414595c183534a
danchitnis <danchitnis@users.noreply.github.com>
2020-10-23 00:19:03 +0100

    removed misc_time patch

--- emcc/run.sh
+++ emcc/run.sh
@@ -20,8 +20,6 @@ cd /opt
 
 git clone $NGSPICE_HOME ngspice-ngspice
 
-cp ./misc_time.c ./ngspice-ngspice/src/misc
-
 cd ngspice-ngspice
 
 #https://www.cyberciti.biz/faq/how-to-use-sed-to-find-and-replace-text-in-files-in-linux-unix-shell/
